1fcfb680ac3c3aeed67f65c2fb5b0645b505ab21
Riyaaz Singh <94745207+Riyaazct@users.noreply.github.com>
2022-10-16 21:58:33 +0200

    complete level 2

--- src/ScoreCard.js
+++ src/ScoreCard.js
@@ -1,6 +1,12 @@
 import HighScoreTable from "./HighScoreTable";
 import allCountryScores from "./scores";
 
+allCountryScores.sort((a, b) => {
+  let textA = a.name.toLowerCase();
+  let textB = b.name.toLowerCase();
+  return textA < textB ? -1 : textA > textB ? 1 : 0;
+});
+
 const scores = allCountryScores.map((score, i) => {
   return <HighScoreTable key={i} data={allCountryScores[i]} />;
 });
