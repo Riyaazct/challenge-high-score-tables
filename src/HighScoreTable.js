8827e8c2f9682613e17cda51144e55522f007ed4
Riyaaz Singh <94745207+Riyaazct@users.noreply.github.com>
2022-10-19 00:20:51 +0200

    update project

--- src/HighScoreTable.js
+++ src/HighScoreTable.js
@@ -2,44 +2,53 @@ import { useState } from "react";
 import allCountryScores from "./scores";
 
 // sort countries alphabetically
-allCountryScores.sort((a, b) => {
-  let textA = a.name.toLowerCase();
-  let textB = b.name.toLowerCase();
-  return textA < textB ? -1 : textA > textB ? 1 : 0;
-});
+// allCountryScores.sort((a, b) => {
+//   let textA = a.name.toLowerCase();
+//   let textB = b.name.toLowerCase();
+//   return textA < textB ? -1 : textA > textB ? 1 : 0;
+// });
 
 let HighScoreTable = () => {
   const [data, setdata] = useState(allCountryScores);
   const [order, setorder] = useState("ASC");
+  const [row, setrow] = useState();
 
-  let [{ scores }] = data;
+  let [{ name, scores }] = data;
 
-  let arrayOfScores = data.map(({ scores }, i) =>
-    scores.map((p) => p.s)
-  );
-
-  // const sorting = (col) => {
-  //   if (order === "ASC") {
-  //     const sorted = [...data].sort((a, b) => (a < b ? -1 : 1));
-
-  //     setdata(sorted);
-  //     setorder("DSC");
-  //   }
-  //   if (order === "DSC") {
-  //     const sorted = [...data].sort((a, b) => (a > b ? 1 : -1));
-  //     setdata(sorted);
-  //     setorder("ASC");
-  //   }
-  // };
+  const sorting = (col) => {
+    console.log(col);
+    if (order === "ASC") {
+      const sorted = [...data].sort((a, b) =>
+        a[col] > b[col] ? 1 : -1
+      );
+      setdata(sorted);
+      setorder("DSC");
+    }
+    if (order === "DSC") {
+      const sorted = [...data].sort((a, b) =>
+        a[col] < b[col] ? 1 : -1
+      );
+      setdata(sorted);
+      setorder("ASC");
+    }
+  };
 
   return (
     <>
-      <button onClick={() => {}}>Click</button>
+      <button className="button" onClick={() => sorting({ scores })}>
+        Sort
+      </button>
       {data.map(({ name, scores }, index) => {
         return (
           <div className="table-container" key={index}>
             <h3>{name}</h3>
-            <table className="table">
+            <table className="table table-striped table-dark">
+              <thead>
+                <tr>
+                  <th onClick={() => sorting("Name")}>Name</th>
+                  <th onClick={() => sorting("Score")}>Score</th>
+                </tr>
+              </thead>
               <tbody>
                 {scores.map(({ n, s }, i) => (
                   <tr key={i}>
--- src/ToggleButton.js
+++
@@ -1,6 +0,0 @@
-const ToggleButton = ({ sort }) => {
-  console.log(sort);
-  return <button onClick={sort}>Click me</button>;
-};
-
-export default ToggleButton;
